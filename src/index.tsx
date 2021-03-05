fa5abbf6d4bc2aa9384e1ea145924cfb6b5e5485
Qolim <liming12679@126.com>
2021-03-06 00:03:53 +0800

    0.0.1

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+/*
+ * @Author: LimingQi
+ * @Date: 2021-03-05 23:05:33
+ * @LastEditTime: 2021-03-05 23:48:10
+ * @LastEditors: LimingQi
+ * @Description:入口
+ * @FilePath: /action-hooks/src/index.tsx
+ * Github: https://github.com/Qolim
+ */
+import useAction from './use-action';
+
+export default {
+  useAction
+}
---
+++ src/use-action/index.ts
@@ -0,0 +1,35 @@
+/*
+ * @Author: LimingQi
+ * @Date: 2021-03-05 23:07:38
+ * @LastEditTime: 2021-03-05 23:47:40
+ * @LastEditors: LimingQi
+ * @Description: useAction hook
+ * @FilePath: /action-hooks/src/use-action/index.ts
+ * Github: https://github.com/Qolim
+ */
+import * as React from 'react';
+
+/**
+ * 事件-状态 绑定hook
+ * @param action 需要执行的事件逻辑，接受的第一个参数为原状态值，后续的参数为事件接收的参数，返回跟新后的状态值
+ * @param defaultState 默认状态值
+ * @returns 返回一个元祖 第一项为执行绑定的事件函数，第二项为可用于监听的状态值
+ */
+export default function useAction<S = undefined, A extends Array<any> = any[]>(
+  action: (state: S, ...args: A) => S,
+  defaultState: S,
+): [
+    (...args: A) => void,
+    S
+  ] {
+
+  const [state, set_state] = React.useState<S>(defaultState)
+
+  const actionBack = (...args: A) => { set_state(action(state, ...args)) }
+
+  return [
+    actionBack,
+    state
+  ]
+
+}
